58caa05916e5b960378324f5400105c83535c456
xavialex <xavialex7@gmail.com>
2018-05-24 17:26:32 +0200

    Webcam script modified

--- instance_segmentation_webcam.py
+++ instance_segmentation_webcam.py
@@ -141,58 +141,6 @@ def visualize_results(image, output_dict):
     
     return image
 
-def detect_objects(image_np, sess, detection_graph):
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    image_np_expanded = np.expand_dims(image_np, axis=0)
-    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-
-    # Each box represents a part of the image where a particular object was detected
-    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-    # Each mask represents a class instance detected in the frame
-    masks = detection_graph.get_tensor_by_name('detection_masks:0')
-
-    # Each score represent how level of confidence for each of the objects.
-    # Score is shown on the result image, together with the class label.
-    scores = detection_graph.get_tensor_by_name('detection_scores:0')
-    classes = detection_graph.get_tensor_by_name('detection_classes:0')
-    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-    
-    # Actual detection
-    (boxes, masks, scores, classes, num_detections) = sess.run(
-        [boxes, masks, scores, classes, num_detections],
-        feed_dict={image_tensor: image_np_expanded})
-    
-    # Print every class + probability prediction
-    classes_np = np.squeeze(classes).astype(np.int32)
-    #print(classes_np)
-    scores_np = np.squeeze(scores)
-    #print("Scores 1\n")
-    #print(scores_np)
-    
-#    # Draw just class 1 detections (Person)
-#    total_people = 0 # Total observations per frame
-#    for i in range(classes_np.size):
-#        if classes_np[i]==1 and scores_np[i]>=0.5:
-#            total_people += 1
-#        elif classes_np[i] != 1:
-#            scores_np[i] = 0.02
-#    print("######################### " + str(total_people) + " ########################")
-    #print("Scores 2\n")
-    #print(scores_np)
-    
-    # Visualization of the results of a detection
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        np.squeeze(boxes),
-        np.squeeze(classes).astype(np.int32),
-        scores_np,
-        category_index,
-        instance_masks=np.squeeze(masks), 
-        use_normalized_coordinates=True,
-        min_score_thresh=.5, 
-        line_thickness=2)
-    return image_np
-
     
 def main():
     video_capture = cv2.VideoCapture(0)
